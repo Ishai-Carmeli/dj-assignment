9be24f14c831f504cd7a34a0aaed853274ff8c69
IshaiCarmeli <ishaic@post.bgu.ac.il>
2025-12-01 19:57:08 +0000

    Add: loadTrackToCache() implementation

--- src/DJControllerService.cpp
+++ src/DJControllerService.cpp
@@ -10,8 +10,26 @@ DJControllerService::DJControllerService(size_t cache_size)
  * TODO: Implement loadTrackToCache method
  */
 int DJControllerService::loadTrackToCache(AudioTrack& track) {
-    // Your implementation here 
-    return 0; // Placeholder
+    if (cache.contains(track.get_title())) {
+        cache.get(track.get_title());
+        return 1;
+    }
+
+    PointerWrapper<AudioTrack> cloned_track = track.clone();
+    AudioTrack* raw_ptr = cloned_track.release();
+    if (raw_ptr == nullptr) {
+        std::cerr << "[ERROR] Can't load a null track" << std::endl;
+        return 0;
+    }
+
+    raw_ptr->load();
+    raw_ptr->analyze_beatgrid();
+    bool put_result = cache.put(PointerWrapper<AudioTrack>(raw_ptr));
+    if (put_result) {
+        return -1;
+    }
+
+    return 0; 
 }
 
 void DJControllerService::set_cache_size(size_t new_size) {
